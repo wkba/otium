0d94905366a75e9b86b2f158740e02defba44b3c
Wakabayashi syunsuke <wkba61@gmail.com>
2016-04-17 20:30:58 +0900

    first

---
+++ otium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  otium
+//
+//  Created by 若林俊輔 on 2016/04/17.
+//  Copyright © 2016年 p6iwi6q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
