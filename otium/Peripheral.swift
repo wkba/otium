d789d90e7e06bd1447ca421c731a67f3187115eb
Wakabayashi syunsuke <wkba61@gmail.com>
2016-04-17 21:22:50 +0900

    central  peripherの実装

---
+++ otium/Peripheral.swift
@@ -0,0 +1,62 @@
+//
+//  Peripheral.swift
+//  otium
+//
+//  Created by 若林俊輔 on 2016/04/17.
+//  Copyright © 2016年 p6iwi6q. All rights reserved.
+//
+import CoreBluetooth
+import CoreLocation
+
+class Peripheral: CBPeripheralManager {
+    
+    private static let sharedInstance = Peripheral()
+    private let beaconIdentifier = ""
+    private let uuidString = "aaaabbbb-aaaa-aaaa-aaaa-aaaabbbbcccc"
+    private let major: CLBeaconMajorValue = CLBeaconMajorValue(arc4random() % 100)
+    private let minor: CLBeaconMinorValue = CLBeaconMinorValue(arc4random() % 3)
+    
+    static func startAdvertising() {
+        sharedInstance.delegate = sharedInstance
+    }
+}
+
+
+extension Peripheral: CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .Unknown:
+            print("Unknown")
+        case .Resetting:
+            print("Resetting")
+        case .Unsupported:
+            print("Unsupported")
+        case .Unauthorized:
+            print("Unauthorized")
+        case .PoweredOff:
+            print("PoweredOff")
+        case .PoweredOn:
+            print("PoweredOn")
+            startAdvertisingWithPeripheralManager(peripheral)
+        }
+    }
+    
+    func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager, error: NSError?) {
+        if let error = error {
+            print("Failed to start advertising with error:\(error)")
+        } else {
+            print("Start advertising")
+        }
+    }
+    
+    private func startAdvertisingWithPeripheralManager(manager: CBPeripheralManager) {
+        guard let proximityUUID = NSUUID(UUIDString: uuidString) else {
+            return
+        }
+        
+        let beaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, major: major, minor: minor, identifier: beaconIdentifier)
+        let beaconPeripheralData: NSDictionary = beaconRegion.peripheralDataWithMeasuredPower(nil)
+        manager.startAdvertising(beaconPeripheralData as? [String: AnyObject])
+    }
+}
--- otium/ViewController.swift
+++ otium/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "aaaabbbb-aaaa-aaaa-aaaa-aaaabbbbcccc")!, identifier: "Estimotes")
+    let colors = [
+        0: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        1: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
+        2: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK: central
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue % 3]
+        }
+        print(beacons)
+    }
 
 }
 
