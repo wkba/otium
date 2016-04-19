857fab3f63994af1aaee0821366d41d5220b8e28
Wakabayashi syunsuke <wkba61@gmail.com>
2016-04-19 15:57:10 +0900

    add PulseClass

---
+++ Pods/Target Support Files/Pods-otiumUITests/Pods-otiumUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_otiumUITests : NSObject
+@end
+@implementation PodsDummy_Pods_otiumUITests
+@end
---
+++ otium/Pulse.swift
@@ -0,0 +1,9 @@
+//
+//  Palse.swift
+//  otium
+//
+//  Created by 若林俊輔 on 2016/04/19.
+//  Copyright © 2016年 p6iwi6q. All rights reserved.
+//
+
+import Foundation
